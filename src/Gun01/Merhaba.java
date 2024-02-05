95a3422a29d6c8a29c4ccdadb3c431644336c570
TechnoStudy <ismettechno@gmail.com>
2024-02-05 18:45:19 +0000

    for konusu

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,5 +3,9 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("merhaba");
+        }
     }
 }
